1e17755913a4b244af33f44db525ea8bdb954ff9
Faris-Hmd <88156110+Faris-Hmd@users.noreply.github.com>
2024-06-11 13:44:33 +0000

    sigh

--- component/services.js
+++ component/services.js
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import 'services.css';
 const servicesData = [
   {
     id: 1,
