e3dbfdcf87d163610547e8dcd05fa965d39b76cf
Faris-Hmd <88156110+Faris-Hmd@users.noreply.github.com>
2024-06-30 15:29:38 +0000

    Still on Footer

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -94,7 +94,7 @@ function Footer() {
                         </ul>
                     </div>
                 </Container> */}
-      <Container className="p-1 flex-r bg-liner">
+      <Container className="p-1 flex-r">
         <Row>
           <Col col-6 col-md-2 mb-3>
           <h5>Section</h5>
@@ -111,6 +111,54 @@ function Footer() {
                                         className="nav-link p-0">About</a></li>
                             </ul>
           </Col>
+          <Col col-6 col-md-2 mb-3>
+          <h5>Section</h5>
+                            <ul className="nav flex-column">
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Home</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Features</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Pricing</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">FAQs</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">About</a></li>
+                            </ul>
+          </Col>
+          <Col col-6 col-md-2 mb-3>
+          <h5>Section</h5>
+                            <ul className="nav flex-column">
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Home</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Features</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Pricing</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">FAQs</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">About</a></li>
+                            </ul>
+          </Col>
+          <Col col-md-5 offset-md-1 mb-3>
+                            <form>
+                                <h5>Subscribe to our newsletter</h5>
+                                <p>Monthly digest of what's new and exciting
+                                    from us.</p>
+                                <div
+                                    class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                    <label for="newsletter1"
+                                        class="visually-hidden">Email
+                                        address</label>
+                                    <input id="newsletter1" type="text"
+                                        class="form-control"
+                                        placeholder="Email address"/>
+                                    <button class="btn btn-primary"
+                                        type="button">Subscribe</button>
+                                </div>
+                            </form>
+                        </Col>
         </Row>
         {/* <Col xs={12}>
           <small>
