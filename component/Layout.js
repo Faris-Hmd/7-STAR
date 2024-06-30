ad8e5b27975d9c0689a0ffc3dcf46f4ca2674c0f
Faris-Hmd <88156110+Faris-Hmd@users.noreply.github.com>
2024-06-30 15:08:12 +0000

    Error

--- component/Layout.js
+++ component/Layout.js
@@ -40,70 +40,70 @@ const Layout = ({ children }) => {
             {children}
           </main>
           <footer className="footer bg-dark text-white py-5">
-          <div class="container">
-                    <div class="row">
-                        <div class="col-6 col-md-2 mb-3">
+          <div className="container">
+                    <div className="row">
+                        <div className="col-6 col-md-2 mb-3">
                             <h5>Section</h5>
-                            <ul class="nav flex-column">
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Home</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Features</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Pricing</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">FAQs</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">About</a></li>
+                            <ul className="nav flex-column">
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Home</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Features</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Pricing</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">FAQs</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">About</a></li>
                             </ul>
                         </div>
 
-                        <div class="col-6 col-md-2 mb-3">
+                        <div className="col-6 col-md-2 mb-3">
                             <h5>Section</h5>
-                            <ul class="nav flex-column">
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Home</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Features</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Pricing</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">FAQs</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">About</a></li>
+                            <ul className="nav flex-column">
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Home</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Features</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Pricing</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">FAQs</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">About</a></li>
                             </ul>
                         </div>
 
-                        <div class="col-6 col-md-2 mb-3">
+                        <div className="col-6 col-md-2 mb-3">
                             <h5>Section</h5>
-                            <ul class="nav flex-column">
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Home</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Features</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">Pricing</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">FAQs</a></li>
-                                <li class="nav-item mb-2"><a href="#"
-                                        class="nav-link p-0">About</a></li>
+                            <ul className="nav flex-column">
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Home</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Features</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">Pricing</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">FAQs</a></li>
+                                <li className="nav-item mb-2"><a href="#"
+                                        className="nav-link p-0">About</a></li>
                             </ul>
                         </div>
 
-                        <div class="col-md-5 offset-md-1 mb-3">
+                        <div className="col-md-5 offset-md-1 mb-3">
                             <form>
                                 <h5>Subscribe to our newsletter</h5>
                                 <p>Monthly digest of what's new and exciting
                                     from us.</p>
                                 <div
-                                    class="d-flex flex-column flex-sm-row w-100 gap-2">
+                                    className="d-flex flex-column flex-sm-row w-100 gap-2">
                                     <label for="newsletter1"
-                                        class="visually-hidden">Email
+                                        className="visually-hidden">Email
                                         address</label>
                                     <input id="newsletter1" type="text"
-                                        class="form-control"
+                                        className="form-control"
                                         placeholder="Email address"/>
-                                    <button class="btn btn-primary"
+                                    <button className="btn btn-primary"
                                         type="button">Subscribe</button>
                                 </div>
                             </form>
@@ -111,20 +111,20 @@ const Layout = ({ children }) => {
                     </div>
 
                     <div
-                        class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                        className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
                         <p>&copy; 2024 Company, Inc. All rights reserved.</p>
-                        <ul class="list-unstyled d-flex">
-                            <li class="ms-3"><a class="link-body-emphasis"
-                                    href="#"><svg class="bi" width="24"
+                        <ul className="list-unstyled d-flex">
+                            <li className="ms-3"><a className="link-body-emphasis"
+                                    href="#"><svg className="bi" width="24"
                                         height="24"><use
                                             xlink:href="#twitter" /></svg></a></li>
 
-                            <li class="ms-3"><a class="link-body-emphasis"
-                                    href="#"><svg class="bi" width="24"
+                            <li className="ms-3"><a className="link-body-emphasis"
+                                    href="#"><svg className="bi" width="24"
                                         height="24"><use
                                             xlink:href="#instagram" /></svg></a></li>
-                            <li class="ms-3"><a class="link-body-emphasis"
-                                    href="#"><svg class="bi" width="24"
+                            <li className="ms-3"><a className="link-body-emphasis"
+                                    href="#"><svg className="bi" width="24"
                                         height="24"><use
                                             xlink:href="#facebook" /></svg></a></li>
                         </ul>
