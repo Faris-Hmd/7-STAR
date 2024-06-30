2a02cb1e78312f8600fe40e72e17e28f08764ec8
Faris-Hmd <88156110+Faris-Hmd@users.noreply.github.com>
2024-06-30 16:26:04 +0000

    footer again

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -163,7 +163,7 @@ function Footer() {
                         className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
                                             <center>©2023 by Faris Hamad. created with Next.js</center>
 
-                        {/* <p>&copy; 2024 Company, Inc. All rights reserved.</p>
+                        {/* <p>&copy; 2024 Company, Inc. All rights reserved.</p> */}
                         <ul className="list-unstyled d-flex">
                             <li className="ms-3"><a className="link-body-emphasis"
                                     href="#"><svg className="bi" width="24"
@@ -178,7 +178,7 @@ function Footer() {
                                     href="#"><svg className="bi" width="24"
                                         height="24"><use
                                             xlink:href="#facebook" /></svg></a></li>
-                        </ul> */}
+                        </ul> 
                     </div>
         </Row>
         {/* <Col xs={12}>
